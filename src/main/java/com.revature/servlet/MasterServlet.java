56e043fbc26defd90437a4c8d07fb4e6adaaea71
Grant Wiswell <wiswellgrant@gmail.com>
2020-12-17 11:50:49 -0600

    Minor change to test pipelines

--- src/main/java/com.revature/servlet/MasterServlet.java
+++ src/main/java/com.revature/servlet/MasterServlet.java
@@ -11,7 +11,7 @@ public class MasterServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setStatus(404);
-        resp.getWriter().write("<p>Hello Jenkins!</p>");
+        resp.getWriter().write("<h1>Hello Jenkins! PIPELINES ARE AWESOME!</h1>");
         resp.setStatus(200);
     }
 }
